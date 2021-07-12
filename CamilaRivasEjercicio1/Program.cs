a2431a211b583d85b41f021486029e0af510a637
Camila425 <80554551+Camila425@users.noreply.github.com>
2021-07-12 20:37:04 -0300

    Agregar archivos de proyecto.

---
+++ CamilaRivasEjercicio1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamilaRivasEjercicio1
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			double Perimetro, Superficie, Hipotenusa;
+			double CatetoA, CatetoB;
+			Console.Write("Ingrese la Primer  medida del Triangulo:");
+			CatetoA = Convert.ToDouble(Console.ReadLine());
+			Console.Write("Ingrese la segunda  medida del Triangulo:");
+			CatetoB = Convert.ToDouble(Console.ReadLine());
+
+			Superficie = (CatetoA * CatetoB) / 2;
+			Hipotenusa = Math.Sqrt( Math.Pow ( CatetoA ,2) + Math.Pow( CatetoB , 2));
+			Perimetro = CatetoA + CatetoB + Hipotenusa;
+
+			Console.WriteLine($"El perimetro es:{Perimetro}");
+			Console.WriteLine($"La superficie es:{Superficie}");
+
+			Console.ReadLine();
+
+		}
+    }
+}
